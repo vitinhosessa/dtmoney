927c8fd0527c12776acd5e7e5a60fd7840afeaec
Vitinho <vitinho.dev@gmail.com>
2021-05-12 18:59:14 -0300

    feat: add botão abre modal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,22 @@
-import React from "react"
 import logoImg from "../../Assets/logo.svg"
 import { Container, Content } from "./styles"
-export function Header() {
+
+interface IHeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header({ onOpenNewTransactionModal }: IHeaderProps) {
+
   return (
     <Container>
       <Content>
         <img src={logoImg} alt="DT Money" />
-        <button type="button">Nova Transação</button>
+        <button 
+          type="button"
+          onClick={onOpenNewTransactionModal}
+        >
+          Nova Transação
+        </button>
       </Content>
     </Container>
   )
