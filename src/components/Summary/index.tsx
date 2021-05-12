b4eb5e6358adaf7c1e6b10c7af988b60d09bccab
Vitinho <vitinho.dev@gmail.com>
2021-05-12 15:18:42 -0300

    feat: criado Summary

---
+++ src/components/Summary/index.tsx
@@ -0,0 +1,37 @@
+import incomeImg from "../../Assets/income.svg"
+import outcomeImg from "../../Assets/outcome.svg"
+import totalImg from "../../Assets/total.svg"
+import { Container } from "./styles";
+
+export function Summary() {
+  return (
+    <Container>
+      <div>
+        <header>
+          <p>Entradas</p>
+          <img src={incomeImg} alt="Entradas" />
+        </header>
+
+        <strong>R$100,00</strong>
+      </div>
+
+      <div>
+        <header>
+          <p>Saídas</p>
+          <img src={outcomeImg} alt="Saídas" />
+        </header>
+
+        <strong>- R$50,00</strong>
+      </div>
+
+      <div className="highlight-background">
+        <header>
+          <p>Total</p>
+          <img src={totalImg} alt="Total" />
+        </header>
+
+        <strong>R$50,00</strong>
+      </div>
+    </Container>
+  )
+}
